1b3204352be243e716febead941088c6f57bf9da
muhammad ahdan rafif maulana <rafifmaulana1412@gmail.com>
2025-11-14 19:40:45 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero(){
   return (
     <section id="about" className="py-5">
       <div className="container text-center" data-aos="fade-up">
-        <h1 className="display-5 fw-bold text-info">Hi, I’m Ahdan — Web Developer</h1>
+        <h1 className="display-5 fw-bold text-info">Hi, I'm Ahdan, A passionate Web Developer crafting clean and functional websites.</h1>
         <p className="lead text-secondary mx-auto" style={{maxWidth:800}}>
           I’m a Web Developer who loves turning ideas into interactive and responsive websites. My background in Arabic Literature at the University of Indonesia helps me think critically and communicate clearly, which I now apply in the world of technology. I believe great websites go beyond design — they’re about creating experiences people enjoy. I’m currently leveling up my skills in Fullstack Web Development and crafting projects that are impactful and accessible for everyone.
         </p>
