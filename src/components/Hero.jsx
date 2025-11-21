3a4723cfad3333114a4465268d7077a91f98af0b
muhammad ahdan rafif maulana <rafifmaulana1412@gmail.com>
2025-11-21 16:45:21 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero(){
           I’m a Web Developer who loves turning ideas into interactive and responsive websites. My background in Arabic Literature at the University of Indonesia helps me think critically and communicate clearly, which I now apply in the world of technology. I believe great websites go beyond design — they’re about creating experiences people enjoy. I’m currently leveling up my skills in Fullstack Web Development and crafting projects that are impactful and accessible for everyone.
         </p>
         <div className="d-flex justify-content-center gap-3 mt-4" data-aos="zoom-in" data-aos-delay="200">
-        <a href="/cv-ahdan.pdf"
+        <a href="/CV Muhammad Ahdan Rafif Maulana.pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="btn btn-info px-4"
